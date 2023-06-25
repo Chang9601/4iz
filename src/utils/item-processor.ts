49ea17ff163198726eb99264c138ff56002bd4f8
Chang9601 <changsup96@gmail.com>
2023-06-25 10:19:35 +0900

    선택적 속성 적용

--- src/utils/item-processor.ts
+++ src/utils/item-processor.ts
@@ -4,35 +4,31 @@ import { ValidationErrorMessage } from './validation-error-message';
 import { Pagination } from './constants/pagination';
 
 export class ItemProcessor {
-  @IsOptional()
   @IsNumber()
   @Min(1, { message: ValidationErrorMessage.POSITIVE_NUMBER })
   limit: number = Pagination.LIMIT;
 
-  @IsOptional()
   @IsNumber()
   @Min(1, { message: ValidationErrorMessage.POSITIVE_NUMBER })
   offset: number = Pagination.OFFSET;
 
-  @IsOptional()
   @IsString()
   search: string = Pagination.SEARCH;
 
-  @IsOptional()
   @IsString()
   @Transform(({ value }) => value.toLowerCase())
   sort: string = Pagination.SORT;
 
   @IsOptional()
   @IsString()
-  category: string;
+  category?: string;
 
   @IsOptional()
-  size: string[];
+  size?: string[];
 
   @IsOptional()
-  color: string[];
+  color?: string[];
 
   @IsOptional()
-  gender: string[];
+  gender?: string[];
 }

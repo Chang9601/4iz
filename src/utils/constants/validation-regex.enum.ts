b9a6514200aabe607e44498d4c802483f011169a
Chang9601 <changsup96@gmail.com>
2023-07-12 11:00:40 +0900

    order e2e 테스트

--- src/utils/constants/validation-regex.enum.ts
+++ src/utils/constants/validation-regex.enum.ts
@@ -4,8 +4,8 @@ export const VALIDATION_REGEX = {
   PASSWORD: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*]).{8,15}$/,
   PHONE_NUMBER: /^([0-9]{3})[-]([0-9]{4})[-][0-9]{4}$/,
   STREET:
-    /^[\p{Script=Hangul}\s]{1,}(?:[\p{Script=Hangul}\s]+[\p{Script=Hangul}]){4,}[\p{Script=Hangul}\s]*$/u,
+    /^([\p{Script=Hangul}\s\d]){1,}(?:([\p{Script=Hangul}\s\d])+([\p{Script=Hangul}\d])){2,}([\p{Script=Hangul}\s\d])*$/u,
   ADDRESS:
-    /^[\p{Script=Hangul}\s]{1,}(?:[\p{Script=Hangul}\s]+[\p{Script=Hangul}]){3,}[\p{Script=Hangul}\s]*$/u,
+    /^([\p{Script=Hangul}\s\d]){1,}(?:([\p{Script=Hangul}\s\d])+([\p{Script=Hangul}\d])){2,}([\p{Script=Hangul}\s\d])*$/u,
   ZIPCODE: /^[0-9]{5}$/,
 };
